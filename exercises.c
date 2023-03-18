af418447c0ece83f49ac8ef6f9842cae4a5f8177
Replit user <>
2023-03-18 02:57:53 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -125,5 +125,8 @@ sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
   for (int i = 0; i < c->capacidad; i++)
-    c->datos[i] = (a1+b1)+(a2+b2);
+    if(i % 2 == 0)
+      c->datos[i] = (a1+b1);
+    else c->datos[i] = (a2+b2);
+    
 }
